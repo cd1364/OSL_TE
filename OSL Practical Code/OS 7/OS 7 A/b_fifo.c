05749a2c840cec03f93fe739c7ff56ff4a76b60c
Chaitanya Daphal <chaitanyardaphal1364@gmail.com>
2023-10-29 00:46:18 +0530

    OS 7 B

--- OSL Practical Code/OS 7/OS 7 A/b_fifo.c
+++ OSL Practical Code/OS 7/OS 7 A/b_fifo.c
@@ -1,3 +1,14 @@
+/*
+Assignment No. : 7 A
+
+Name : Daphal Chaitanya Ramdas
+Roll No. : 35015
+Problem Statemnt : FIFOS: Full duplex communication between two independent processes. First process accepts
+sentences and writes on one pipe to be read by second process and second process counts number of
+characters, number of words and number of lines in accepted sentences, writes this output in a text file
+and writes the contents of the file on second pipe to be read by first process and displays on standard
+output.
+*/
 
 
 #include<stdio.h>
